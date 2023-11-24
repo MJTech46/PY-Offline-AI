fabc82865ec48f17ffc856841e207090482624b4
MJTech46 <140804026+MJTech46@users.noreply.github.com>
2023-11-24 22:09:43 +0530

    Create ytapi.py
    decoded the json of ytsteam api

---
+++ ytapi.py
@@ -0,0 +1,45 @@
+import requests
+
+url = "https://ytstream-download-youtube-videos.p.rapidapi.com/dl"
+
+querystring = {"id":"UxxajLWwzqY"}
+
+headers = {
+	"X-RapidAPI-Key": "035f085157msh229319bf92d56f3p122af5jsn241c25adf036",
+	"X-RapidAPI-Host": "ytstream-download-youtube-videos.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring).json()
+
+#response filtering
+title= response['title'] #title
+lengthSeconds = response['lengthSeconds'] #duration in seconds
+thumbnail_temp = response['thumbnail'][1]
+thumbnail= thumbnail_temp['url'] #url of thumbnail
+formats = response['formats'] #video quality list
+adaptiveFormats = response['adaptiveFormats'] #all other qualities including audio
+
+#printing data
+print("Title: ",title)
+print("Duration:", lengthSeconds)
+print(thumbnail)
+
+print('\n\n\n')
+
+for i in formats:
+    print('qualityLabel: ',i['qualityLabel'])
+    print('url: ',i['url'])
+
+print('\n\n\n')
+
+for i in adaptiveFormats:
+    try:
+        print('qualityLabel: ',i['qualityLabel'])
+        print("formate:",i['mimeType'])
+        print('size:',int(i['contentLength'])*10**(-6))
+        print('url: ',i['url'])
+    except:
+        print('audioQuality: ',i['audioQuality'])
+        print("formate:",i['mimeType'])
+        print('size:',int(i['contentLength'])*10**(-6))
+        print('url: ',i['url'])
